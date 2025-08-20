736ce3151b5b3f9ad56d652aaf1329fd7e7527ba
Damilola Bada <damilolabada@Damilolas-Mac-mini.local>
2025-08-20 19:52:08 +0200

    chore: deleted unused icon-set.ts

--- src/components/custom-icon/icons-set.ts
+++
@@ -1,6 +0,0 @@
-import createIconSet from '@expo/vector-icons/createIconSet';
-
-const glyphMap = { 'icon-name': 1234, test: '∆' };
-const CustomIcon = createIconSet(glyphMap, 'fontFamily', 'custom-icons');
-
-export default CustomIcon;
