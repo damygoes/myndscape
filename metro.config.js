a1cd60ea8fef5b1401a34cf6968fd01c11311c13
Damilola Bada <damilolabada@Damilolas-Mac-mini.local>
2025-08-21 12:18:09 +0200

    chore: modified metro config

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,13 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Custom transformer for SVG support
 config.transformer = {
+    ...config.transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
 };
+
+// Custom resolver for SVG and asset support
 config.resolver = {
     ...config.resolver,
     assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
